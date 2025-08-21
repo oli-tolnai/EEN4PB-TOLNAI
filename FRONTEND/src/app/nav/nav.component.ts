bf7a912ef8cc8dd44b5abbf22ff5850b68e72d3d
oli-tolnai <oli.tolnai3@gmail.com>
2025-08-21 17:29:55 +0200

    nav bar button done

--- FRONTEND/src/app/nav/nav.component.ts
+++ FRONTEND/src/app/nav/nav.component.ts
@@ -8,7 +8,9 @@ import { Component } from '@angular/core';
 })
 export class NavComponent {
 
-  
+  isNavbarCollapsed = true;
 
-  
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
 }
