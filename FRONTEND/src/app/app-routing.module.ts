2441088c11584d4050cd6d4d334b858ed4f3344a
oli-tolnai <oli.tolnai3@gmail.com>
2025-09-09 14:14:47 +0200

    app routing module supplemented

--- FRONTEND/src/app/app-routing.module.ts
+++ FRONTEND/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { ListComponent } from './list/list.component';
 import { StatsComponent } from './stats/stats.component';
 import { ViewComponent } from './view/view.component';
 import { CreateProjectComponent } from './create-project/create-project.component';
+import { EditprojComponent } from './editproj/editproj.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "list", pathMatch: "full"},
   { path: "list", component: ListComponent},
-  { path: "stats", component: StatsComponent},
-  { path: "view/:id", component: ViewComponent},
   { path: "create", component: CreateProjectComponent},
+  { path: "view/:id", component: ViewComponent},
+  { path: "editproj/:id", component: EditprojComponent}
+  { path: "stats", component: StatsComponent},
   { path: "**", redirectTo: "list", pathMatch: "full"}
   
 ];
